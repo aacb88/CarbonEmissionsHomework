ee1281a4f4620bdf57d3a1304378ac24d49069a1
aacb88 <arthur.cook@gmail.com>
2016-03-15 17:47:03 -0500

    Uploading completed assignment

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	// When the page is loaded,
 	// call the loadData() function.
-
+    loadData();
 });
 
 
@@ -14,6 +14,10 @@ function loadData() {
 
 	// Write an AJAX call here to load your data.
 	// Then PASS the data to writeTable();
+	$.getJSON("data/sectors.json", function(data){
+		writeTable(data);
+
+	});
 }
 
 
@@ -29,7 +33,25 @@ function writeTable(data) {
 	// 8. Use the DataTables library to make it sortable.
 	// 9. Sort by total emissions, most to least.
 	// 10. Set the default view to show all 50 states plus DC. You'll want to reference the [Data Tables](https://www.datatables.net/) documentation for those last two.
+	for (i=0; i<data.length; i++) {
+				var State = data[i]["state"];
+				var EmsTotal = data[i]["total"];
+				var barWidth = EmsTotal/641.0 *100;
+				
+				if (State != "United States total"){
+				$(".table").append(
+					"<tr>"+"<td class = 'State'>"+State+"</td>"+"<td class = 'Emissions'>"+EmsTotal+"</td>"+"<td class = 'bColumn'>"+"<div class ='bar' style='width: "+barWidth+"px'>"+"</div>"+"</td>"+"</tr>"
 
+					);
+             }
 
+	}	
+
+		$('.emissions').DataTable(
+		     {
+		     	"order" : [[1, "desc"]],
+		     	"lengthMenu":[[100], ["All"]],
+		     }
+	);
 }
 
